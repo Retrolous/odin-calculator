2540c0bcc1fc78a6413e24fe1dbf80bc2a116aa1
retrolous <lukaspetkunas07@gmail.com>
2024-08-08 21:52:10 +0300

    Add subtract function

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ function add(...addArgs){
     return addArgs.reduce((a, b) => a + b, 0)
 }
 
+function subtract(...subtractArgs){
+    return subtractArgs.reduce((a, b) => a - b)
+}
+
 console.log(add(1, 2, 3))
+console.log(subtract(1, 10, 3))
