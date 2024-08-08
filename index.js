cc0836448d60f9135a9bcc1fd6774159618ecdfd
retrolous <lukaspetkunas07@gmail.com>
2024-08-08 21:50:25 +0300

    Add add function

--- index.js
+++ index.js
@@ -1,42 +1,5 @@
-function filterRange(arr, a, b){
-    return arr.filter((input) => input >= a && input <= b);
+function add(...addArgs){
+    return addArgs.reduce((a, b) => a + b, 0)
 }
 
-function filterRangeInPlace(arr, a, b){
-    arr.foreach((item) => {if(a < item || b > item){arr.splice(1, 1)}})
-}
-
-function sort(arr){
-    arr.sort((a, b) => b - a)
-}
-
-function sorter(arr){
-    arr.slice().sort();
-}
-
-function namer(arr){
-    arr.map((item) => item.name)
-}
-
-function sortByAge(users){
-    users.sort((a, b) => a.age - b.age)
-}
-
-function shuffleArray(arr){
-    arr.sort((a, b) => Math.random() - 0.5)
-}
-
-function avgAge(arr){
-    sum = arr.reduce((prev, current) => prev + current.age, 0);
-    sum /= arr.length;
-}
-
-function unique(arr){
-    let uniqueArr = [];
-    for(let i = 0; i < arr.length; i++){
-        if(!uniqueArr.includes(arr[i])){
-            uniqueArr.push(arr[i])
-        }
-    }
-    return uniqueArr;
-}
+console.log(add(1, 2, 3))
