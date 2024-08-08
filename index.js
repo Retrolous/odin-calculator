0cd01a9dd06e8ba5161db1d08e0977ec1c035efd
retrolous <lukaspetkunas07@gmail.com>
2024-08-08 21:54:21 +0300

    Add multiply and divide  functions

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-function add(...addArgs){
-    return addArgs.reduce((a, b) => a + b, 0)
+function add(...addArgs) {
+  return addArgs.reduce((a, b) => a + b, 0);
 }
 
-function subtract(...subtractArgs){
-    return subtractArgs.reduce((a, b) => a - b)
+function subtract(...subtractArgs) {
+  return subtractArgs.reduce((a, b) => a - b);
 }
 
-console.log(add(1, 2, 3))
-console.log(subtract(1, 10, 3))
+function multiply(...multiplyArgs) {
+  return multiplyArgs.reduce((a, b) => a * b, 1);
+}
+
+function divide(...divideArgs) {
+  return divideArgs.reduce((a, b) => a / b);
+}
+
+console.log(add(1, 2, 3));
+console.log(subtract(1, 10, 3));
+console.log(multiply(1, 10, 3));
+console.log(divide(1, 10, 3));
